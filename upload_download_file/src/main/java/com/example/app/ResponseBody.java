84170fe7198dfe200bdb41ac181d46b5be319b00
Akshat Srivastava <Akshat Srivastava@DESKTOP-FPQ4SKI>
2022-12-11 22:17:07 +0530

    file  upload

---
+++ upload_download_file/src/main/java/com/example/app/ResponseBody.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class ResponseBody {
+	
+	private String Status;
+	private T result;
+	private T error;
+	
+	
+
+}
